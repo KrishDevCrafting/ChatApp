e421713e789f68457d08146cf4f731bc1c5a33ab
KrishDevCrafting <krishkumer150@gmail.com>
2025-06-07 23:28:50 +0530

    react-component

---
+++ Frontend/my-app/src/app/components/navBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <navbar>
+        <section>
+          <ul>
+            <li>Home</li>
+            <li>General</li>
+            <li>Products</li>
+          </ul>
+
+          <div>
+            <ul>
+              <li>Login-btn</li>
+            </ul>
+          </div>
+        </section>
+      </navbar>
+    </>
+  );
+};
+
+export default Navbar;
