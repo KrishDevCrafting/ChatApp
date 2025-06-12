ae64d68ab74ac77254461d67ebf1d41b1e686a27
KrishDevCrafting <krishkumer150@gmail.com>
2025-06-13 00:10:39 +0530

    clerk-error

--- Frontend/my-app/middleware.js
+++ Frontend/my-app/middleware.js
@@ -1,16 +1,20 @@
+// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
+  console.log("Middleware path:", req.nextUrl.pathname);
+
+  if (!isPublicRoute(req)) {
+    await auth().protect(); // will redirect if not signed in
+  }
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|favicon.ico|.*\\..*).*)"],
 };
--- Frontend/my-app/src/app/layout.tsx
+++ Frontend/my-app/src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Navbar />
           <Page />
-          
+          {children}
         </body>
       </html>
     </ClerkProvider>
